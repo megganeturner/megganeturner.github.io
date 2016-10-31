4bcb92e8d6358ccfebbe69e56c792b0d95374840
megganeturner <meggan.turner@hotmail.com>
2016-10-31 22:56:36 +1100

    modules sliding

---
+++ js/main.js
@@ -0,0 +1,26 @@
+$(document).ready(function(){
+  $('#about').click(function(){
+    $('.showing').not('.bio').toggle('slide', {direction:'down'}, 800).toggleClass('showing');
+    $('.bio.top').toggle('slide',{direction:'down'}, 800).toggleClass('showing');
+    $('.bio.bottom').toggle('slide', {direction:'up'}, 800).toggleClass('showing');
+  });
+
+  $('#contact').click(function(){
+    $('.showing').not('.contact').toggle('slide', {direction:'down'}, 800).toggleClass('showing');
+    $('.contact').toggle('slide', {direction:'up'}, 800).toggleClass('showing');
+  });
+
+  $('#skills').click(function(){
+    $('.showing').not('.skills').toggle('slide', {direction:'down'}, 800).toggleClass('showing');
+    $('.skills').toggle('slide', {direction:'up'}, 800).toggleClass('showing');
+  });
+
+  $('#work').click(function(){
+    $('.showing').not('.work').toggle('slide', {direction:'down'}, 800).toggleClass('showing');
+    $('.work').toggle('slide', {direction: 'left'}, 1000).toggleClass('showing');
+  });
+
+  $('.title').click(function(){
+    $('.showing').toggle('slide', {direction:'down'},800).toggleClass('showing');
+  });
+});
