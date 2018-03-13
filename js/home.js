c8d3ef4bbbdd4791830cdf931bb1666c4c326aa8
megganeturner <megganeturner@gmail.com>
2018-03-13 21:34:44 +1100

    include projects.json & implement ajax request

--- js/home.js
+++ js/home.js
@@ -1,1 +1,27 @@
+document.addEventListener("DOMContentLoaded", function() {
+  loadJSON('js/projects.json', function (data) {
+    data.forEach((element) => {
+      console.log(element);
+    });
+  });
 
+});
+
+
+function loadJSON(url, callback) {
+  const request = new XMLHttpRequest();
+  request.open('GET', url, true);
+
+  request.onload = function() {
+    if (request.status >= 200 && request.status < 400) {
+      const data = JSON.parse(request.responseText);
+      return callback(data);
+    } else {
+      console.log('We reached our target server, but it returned an error');
+    }
+  }
+  request.onerror = function() {
+    console.log('There was a connection error of some sort');
+  };
+  request.send();
+}
